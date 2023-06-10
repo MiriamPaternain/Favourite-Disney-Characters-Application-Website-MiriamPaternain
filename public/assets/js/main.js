32ae706d659fe407397cbec22f6a21a98ff890f7
MiriamPaternain <miriampaternain@gmail.com>
2023-06-10 16:25:37 +0200

    add event to characters

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -16,13 +16,28 @@ function renderCharacterList (listData){
     for(const eachCharacter of listData){
         charactersList.innerHTML += renderCharacter(eachCharacter);
     }
+    addEventCharacter();
 }
+
+function addEventCharacter(){
+    const allLiElements = document.querySelectorAll('.js_li');
+    for(const eachLi of allLiElements){
+        eachLi.addEventListener("click", handleClick);
+    }
+}
+
 
 function renderCharacter(data){
 let html = `<li id = "${data._id}" class="characters_container--li js_li">
-          <img src="${data.imageUrl}" class="img js_li--img" alt=""/>
+          <img src="${data.imageUrl}"/>
           <p class="name js_li--name">"${data.name}"</p>
         </li>`;
         return html;
 }
+
+function handleClick (event){
+    const id = event.currentTarget.id;
+    console.log(id);
+}
+
 //# sourceMappingURL=main.js.map
