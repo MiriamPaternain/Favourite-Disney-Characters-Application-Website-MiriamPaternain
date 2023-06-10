32aa0a2c92be206821b6a9aa549b1c6e971eac5a
MiriamPaternain <miriampaternain@gmail.com>
2023-06-10 13:27:08 +0200

    added render characters list

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,10 +1,28 @@
 'use strict';
 
-const characterList = document.querySelector('js_characters_list');
+const charactersList = document.querySelector('.js_characters_list');
+
+let listCharactersApi = [];
 
 fetch('https://api.disneyapi.dev/character')
 .then((response)=> response.json())
 .then((data) => {
     console.log(data);
+    listCharactersApi = data.data;
+    renderCharacterList(listCharactersApi);
 });
+
+function renderCharacterList (listData){
+    for(const eachCharacter of listData){
+        charactersList.innerHTML += renderCharacter(eachCharacter);
+    }
+}
+
+function renderCharacter(data){
+let html = `<li id = "${data._id}" class="js_li">
+          <img src="${data.imageUrl}" class="img js_li--img" alt=""/>
+          <p class="name js_li--name">"${data.name}"</p>
+        </li>`;
+        return html;
+}
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,27 @@
 'use strict';
 
-const characterList = document.querySelector('js_characters_list');
+const charactersList = document.querySelector('.js_characters_list');
+
+let listCharactersApi = [];
 
 fetch('https://api.disneyapi.dev/character')
 .then((response)=> response.json())
 .then((data) => {
     console.log(data);
+    listCharactersApi = data.data;
+    renderCharacterList(listCharactersApi);
 });
+
+function renderCharacterList (listData){
+    for(const eachCharacter of listData){
+        charactersList.innerHTML += renderCharacter(eachCharacter);
+    }
+}
+
+function renderCharacter(data){
+let html = `<li id = "${data._id}" class="js_li">
+          <img src="${data.imageUrl}" class="img js_li--img" alt=""/>
+          <p class="name js_li--name">"${data.name}"</p>
+        </li>`;
+        return html;
+}
