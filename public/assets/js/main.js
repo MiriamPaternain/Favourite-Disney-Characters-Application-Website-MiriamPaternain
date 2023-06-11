fea0f9eb80fd8faa963d8097598751e4239ce120
MiriamPaternain <miriampaternain@gmail.com>
2023-06-11 10:32:52 +0200

    added search function

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -59,4 +59,17 @@ function renderFavListCharacter(){
         ulFavCharactersList.innerHTML += renderCharacter(eachFavCharacter)
     }
 }
+'use strict';
+const inputSearch = document.querySelector('.js_text_input');
+const btnSearch = document.querySelector('.js_search_btn');
+
+const handleClickBtn = (event) => {
+    event.preventDefault();
+    const valueSearch = inputSearch.value;
+    const filterApiList = listCharactersApi.filter((character) =>
+    character.name.toLowerCase().includes(valueSearch.toLowerCase()));
+    renderCharacterList(filterApiList);
+}
+
+btnSearch.addEventListener('click', handleClickBtn);
 //# sourceMappingURL=main.js.map
---
+++ src/js/search.js
@@ -0,0 +1,13 @@
+'use strict';
+const inputSearch = document.querySelector('.js_text_input');
+const btnSearch = document.querySelector('.js_search_btn');
+
+const handleClickBtn = (event) => {
+    event.preventDefault();
+    const valueSearch = inputSearch.value;
+    const filterApiList = listCharactersApi.filter((character) =>
+    character.name.toLowerCase().includes(valueSearch.toLowerCase()));
+    renderCharacterList(filterApiList);
+}
+
+btnSearch.addEventListener('click', handleClickBtn);
